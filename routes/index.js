836a4d97edfb0c14276d9671615fbfe0bb6925e7
Aziz Mezzi <azizmezzi3@gmail.com>
2020-08-21 14:19:26 +0200

    ProjectusingBesuV1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const ControllerMethods = require('../controller/tontine');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/getParticipant', ControllerMethods.getParticipant);
+router.get('/getTontine', ControllerMethods.getTontine);
+
+
+router.post('/createTontine', ControllerMethods.createTontine);
+router.post('/addParticipant', ControllerMethods.addParticipant);
 
 module.exports = router;
